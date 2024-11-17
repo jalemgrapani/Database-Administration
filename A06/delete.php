a0de288ab987a35e2c9eb698804837edef51b908
jalemgrapani <jalemlouisegrapani@gmail.com>
2024-11-17 10:03:20 +0800

    Added modified Files

--- A06/delete.php
+++ A06/delete.php
@@ -2,14 +2,14 @@
 include("connect.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['user_id'])) {
-  $user_id = intval($_POST['user_id']);  // Retrieve the userInfoID value passed from the form
+  $user_id = intval($_POST['user_id']); 
 
-  // SQL query to delete the user, where userInfoID matches the passed value
+ 
   $query = "DELETE FROM userinfo WHERE userInfoID = $user_id";
-  $result = executeQuery($query);  // Execute the delete query
+  $result = executeQuery($query);  
 
   if ($result) {
-    header("Location: index.php"); // Redirect to the index page after successful deletion
+    header("Location: index.php"); 
     exit();
   } else {
     echo "Error deleting record: " . mysqli_error($conn);
