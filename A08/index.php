a711604318d1054d81a3990faaf59d059c0334e1
jalemgrapani <jalemlouisegrapani@gmail.com>
2024-12-04 17:55:31 +0800

    Added files

--- A08/index.php
+++ A08/index.php
@@ -14,6 +14,30 @@ $flightLogsResults = executeQuery($flightLogsQuery);
   <title>Flight Logs</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <style>
+    body {
+      background-color: #f8f9fa;
+    }
+    .card {
+      border: 1px solid #dee2e6;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
+    .flightTable th {
+      background-color: #343a40;
+      color: white;
+      padding: 12px;
+      text-align: center;
+      vertical-align: middle;
+      font-size: 18px;
+    }
+    .flightTable td {
+      padding: 10px;
+      color: #212529;
+      border-bottom: 1px solid #dee2e6;
+      text-align: center;
+      vertical-align: middle;
+    }
+  </style>
 </head>
 
 <body>
@@ -21,7 +45,8 @@ $flightLogsResults = executeQuery($flightLogsQuery);
     <div class="row my-5">
       <div class="col">
         <div class="card p-4 rounded-5">
-          <table class="table">
+          <h1 class="text-center mb-4">Flight Logs</h1>
+          <table class="table flightTable">
             <thead>
               <tr>
                 <th scope="col">Flight Number</th>
@@ -40,20 +65,20 @@ $flightLogsResults = executeQuery($flightLogsQuery);
             <tbody>
               <?php
               if (mysqli_num_rows($flightLogsResults) > 0) {
-                while ($row = mysqli_fetch_assoc($flightLogsResults)) {
+                while ($flightsrow = mysqli_fetch_assoc($flightLogsResults)) {
                   ?>
                   <tr>
-                    <td><?php echo $row['flightNumber']; ?></td>
-                    <td><?php echo $row['departureAirportCode']; ?></td>
-                    <td><?php echo $row['arrivalAirportCode']; ?></td>
-                    <td><?php echo $row['departureDateTime']; ?></td>
-                    <td><?php echo $row['arrivalDateTime']; ?></td>
-                    <td><?php echo $row['flightDurationMinutes']; ?></td>
-                    <td><?php echo $row['airlineName']; ?></td>
-                    <td><?php echo $row['aircraftType']; ?></td>
-                    <td><?php echo $row['passengerCount']; ?></td>
-                    <td><?php echo $row['ticketPrice']; ?></td>
-                    <td><?php echo $row['pilotName']; ?></td>
+                    <td><?php echo $flightsrow['flightNumber']; ?></td>
+                    <td><?php echo $flightsrow['departureAirportCode']; ?></td>
+                    <td><?php echo $flightsrow['arrivalAirportCode']; ?></td>
+                    <td><?php echo $flightsrow['departureDateTime']; ?></td>
+                    <td><?php echo $flightsrow['arrivalDateTime']; ?></td>
+                    <td><?php echo $flightsrow['flightDurationMinutes']; ?></td>
+                    <td><?php echo $flightsrow['airlineName']; ?></td>
+                    <td><?php echo $flightsrow['aircraftType']; ?></td>
+                    <td><?php echo $flightsrow['passengerCount']; ?></td>
+                    <td><?php echo $flightsrow['ticketPrice']; ?></td>
+                    <td><?php echo $flightsrow['pilotName']; ?></td>
                   </tr>
                   <?php
                 }
